daade0ef8277c243f7b403f8826aa08f76422a0a
Dani Miliukov <milukovdaniil11@gmail.com>
2023-07-02 00:57:14 +0300

    .

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { Profile } from 'components/profile/Profile';
+import user from '../components/profile/user.json';
+
+
+import { Statistics } from 'components/statistics/Statistics';
+import data from '../components/statistics/data.json';
+
+import { FriendList } from 'components/friend-list/FriendList';
+import friends from '../components/friend-list/friends.json';
+
+import { TransactionHistory } from 'components/transaction-history/TransactionHistory';
+import transactions from '../components/transaction-history/transactions.json';
 export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+       <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 };
